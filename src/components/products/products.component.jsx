fdb481d3eb4be11fa626a1d5630bb9300a9cd8c2
TKssk01 <takahiro.sasaki.j@gmail.com>
2024-09-26 14:27:31 -0400

    editing productspage

--- src/components/products/products.component.jsx
+++ src/components/products/products.component.jsx
@@ -79,7 +79,7 @@ const Products = () => {
             <img src={ProductImage} alt="MFTO Visual Representation" className="products-main-image" />
             <div className="demo-button-container">
               <a 
-                href="https://sphere-navy.vercel.app/" 
+                href="https://www.sphere-trade.cloud" 
                 className="demo-button" 
                 target="_blank" 
                 rel="noopener noreferrer"
